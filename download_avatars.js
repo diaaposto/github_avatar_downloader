369f96f33ee06afd0e0aa74ff03c6433f77d0d8b
Dia Apostolakis <dia.apostolakis@icloud.com>
2018-08-06 23:13:31 +0000

    json parse

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,32 @@
 var request = require('request');
+var secrets = require('./secrets.js');
+
+console.log('Welcome to the GitHub Avatar Downloader!');
+// console.log(secrets);
+//how do i retrieve the token in the object, take what's in the key
+// console.log(secrets.GITHUB_TOKEN);
 
 function getRepoContributors(repoOwner, repoName, cb) {
+
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': 'token ' + secrets.GITHUB_TOKEN,
+    }
+  };
+
+  console.log(repoOwner)
+
+  request(options, function(err, res, body) {
+    cb(err, JSON.parse(body));
+  });
+}
+
 
   getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
 
-}
+
